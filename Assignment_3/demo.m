04a1585cc35ea14c95780542ec6051147105e1d3
Juan Buhagiar <juan.buhagiar@etpa.nl>
2018-03-06 15:40:03 +0100

    addition of demo

--- Assignment_3/demo.m
+++ Assignment_3/demo.m
@@ -1,18 +1,16 @@
-function res
-image = imread('pingpong/0000.jpeg');
-image2 = imrotate(image,45);
-[H,Ix,Iy,y,x] = harris_corner_detector(image2, 3.524088152432112e+07/5 , 15,0.35, 3 ); 
+function demo(image)
+% Using average because of 6th param
+[H,Ix,Iy,y,x] = harris_corner_detector(image, 0, 15, 0.35, 3, 2); 
 
 subplot(1,3,1);
-imshow(image2) 
+imshow(image) 
 title('harris corner detection image rotated 45 degrees');
 hold on
 plot(y(:), x(:), 'ro');
 hold off
 image2 = imrotate(image,90);
 
-[H,Ix,Iy,y,x] = harris_corner_detector( image2, 3.524088152432112e+07/5, 15,0.35, 3 );
-
+[H,Ix,Iy,y,x] = harris_corner_detector(image2, 0, 15, 0.35, 3, 2);
 subplot(1,3,2);
 imshow(image2) 
 title('harris corner detection image rotated 90 degrees');
@@ -22,7 +20,7 @@ plot(y(:), x(:), 'ro');
 hold off
 image2 = imrotate(image,135);
 
-[H,Ix,Iy,y,x] = harris_corner_detector( image2, 3.524088152432112e+07/5, 15,0.35, 3 );
+[H,Ix,Iy,y,x] = harris_corner_detector(image2, 0, 15, 0.35, 3, 2);
 
 subplot(1,3,3);
 imshow(image2) 
@@ -31,10 +29,4 @@ title('harris corner detection image rotated 135 degrees');
 hold on
 plot(y(:), x(:), 'ro');
 hold off
-
-
-% C = corner(rgb2gray(image));
-% imshow(image);
-% hold on
-% plot(C(:,1), C(:,2), 'r*');
 end
